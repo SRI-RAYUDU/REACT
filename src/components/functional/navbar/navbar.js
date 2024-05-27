c9a0e0b71e3fd894ed8e7247fa362be877993fd1
sri582 <srikrishnar007@gmail.com>
2024-05-27 12:40:10 +0530

    o

---
+++ src/components/functional/navbar/navbar.js
@@ -0,0 +1,46 @@
+import { Link } from 'react-router-dom';
+
+const NavBar = () => {
+  const darkTheme = false;
+
+  const linkStyle = {
+    textDecoration: 'none',
+    color: 'black',
+  };
+  return (
+    <nav
+      class={
+        darkTheme
+          ? 'navbar navbar-expand-sm bg-dark navbar-dark'
+          : 'navbar navbar-expand-sm bg-light navbar-light'
+      }
+    >
+      <div className="container-fluid">
+        <ul className="navbar-nav">
+          <li className="nav-item nav-link">
+            <Link to={'/'} style={linkStyle}>
+              Home
+            </Link>
+          </li>
+          <li className="nav-item nav-link">
+            <Link to={'/about'} style={linkStyle}>
+              About
+            </Link>
+          </li>
+          <li className="nav-item nav-link">
+            <Link to={'/settings'} style={linkStyle}>
+              Settings
+            </Link>
+          </li>
+          <li className="nav-item nav-link">
+            <Link to={'/blog'} style={linkStyle}>
+              Blog
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
